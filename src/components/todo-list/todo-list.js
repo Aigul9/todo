dfb77e11a8cd9e83e77a6884d5bffbee8a8ed831
Aigul <aigul.sibgatulli@mail.ru>
2021-01-07 20:42:32 +0300

    delete items

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,16 +2,18 @@ import React from 'react';
 import TodoListItem from '../todo-list-item';
 import './todo-list.css'
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted }) => {
     const elements = todos.map(({id, ...itemProps}) => {
         return (
             <li
                 key={id}
                 className="list-group-item">
-                    <TodoListItem {...itemProps}/>
+                    <TodoListItem {...itemProps}
+                    onDeleted={() => onDeleted(id)}/>
             </li>
         )
-    })
+    });
+    
     return (
         // remove todo-list class
         <ul className="list-group todo-list">
